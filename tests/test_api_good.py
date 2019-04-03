0d15d3288195753651019dc6bfcd0a4c7ef89d2d
Brian Skinn <brian.skinn@gmail.com>
2019-04-03 13:26:46 -0400

    TEST REVERT: Remove subtests inside xfail tests
    xfail propagation into the test/subtests is incomplete yet.
    
    Definitely seems a difficult problem! Have to xfail each
    subtest individually?

--- tests/test_api_good.py
+++ tests/test_api_good.py
@@ -275,7 +275,7 @@ def test_api_dataobj_datalinefxn(
 @pytest.mark.parametrize(
     "use_bytes", (True, False), ids=(lambda b: "use_bytes_" + str(b))
 )
-def test_api_dataobj_evolvename(use_bytes, res_cmp, subtests):
+def test_api_dataobj_evolvename(use_bytes, res_cmp):
     """Confirm evolving new DataObj instances works properly."""
     inv = soi.Inventory(res_cmp)
     obj = inv.objects[5].as_bytes if use_bytes else inv.objects[5]  # Arbitrary choice
@@ -285,11 +285,8 @@ def test_api_dataobj_evolvename(use_bytes, res_cmp, subtests):
     obj2 = obj.evolve(name=newname)
     obj3 = obj2.evolve(name=oldname)
 
-    with subtests.test(msg="old_new_match"):
-        assert obj == obj3
-
-    with subtests.test(msg="name_is_changed"):
-        assert obj2.name == newname
+    assert obj == obj3
+    assert obj2.name == newname
 
 
 def test_api_inventory_default_none_instantiation(subtests):
