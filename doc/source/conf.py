c678772047ae27aa943bdafba6050c815485e7c3
Brian Skinn <brian.skinn@gmail.com>
2021-02-01 12:38:35 -0500

    Add docs logo and logo image source
    Defnitely would like the logo to be a bit nicer, either with
    a border, or blending more smoothly into the background color
    of the template...

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -70,6 +70,13 @@ modindex_common_prefix = ["sphobjinv."]
 # -- Common epilogue definition  ------------------------------------------------
 
 rst_epilog = r"""
+
+.. raw:: html
+
+    <small><em>Logo adapted from freely-available image at
+    https://heypik.com/images/hand-drawn-cartoon-color-tourist-attraction-sphinx_8ZU4501.html
+    (retrieved July 2019; now defunct).</em></small>
+
 .. |extlink| image:: /_static/extlink.svg
 
 .. |dag| replace:: :math:`^\dagger`
@@ -316,5 +323,6 @@ html_static_path = ['_static']
 # Output file basename
 htmlhelp_basename = "sphobjinv"
 
-# Location of the favicon image
+# Location of the favicon and logo images
 html_favicon = "_static/soi-logo.png"
+html_logo = "_static/soi-logo_duo.png"
