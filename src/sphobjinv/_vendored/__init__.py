8b72510c036f3220095978ee38ddd702c1824886
Brian Skinn <brian.skinn@gmail.com>
2021-12-11 20:40:43 -0500

    Initial pull-in of vendored fuzzywuzzy

---
+++ src/sphobjinv/_vendored/__init__.py
@@ -0,0 +1,28 @@
+r"""``sphobjinv._vendored`` *package definition module*.
+
+``sphobjinv`` is a toolkit for manipulation and inspection of
+Sphinx |objects.inv| files.
+
+Subpackage marker module for vendored packages.
+
+**Author**
+    Brian Skinn (bskinn@alum.mit.edu)
+
+**File Created**
+    11 Dec 2021
+
+**Copyright**
+    \(c) Brian Skinn 2016-2021
+
+**Source Repository**
+    https://github.com/bskinn/sphobjinv
+
+**Documentation**
+    https://sphobjinv.readthedocs.io/en/latest
+
+**License**
+    The MIT License; see |license_txt|_ for full license terms
+
+**Members**
+
+"""
