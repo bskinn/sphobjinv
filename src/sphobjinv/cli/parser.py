13376fc55341498bd105ea95af92400822f09c26
Brian Skinn <brian.skinn@gmail.com>
2026-03-20 00:12:50 -0400

    Fix line length in parser.py

--- src/sphobjinv/cli/parser.py
+++ src/sphobjinv/cli/parser.py
@@ -44,7 +44,8 @@ class PrsConst:
 
     #: Version &c. output blurb
     VER_TXT = (
-        f"\nsphobjinv v{__version__}\n\nCopyright (c) 2016-2026 Brian Skinn and community contributors\n"
+        f"\nsphobjinv v{__version__}\n\n"
+        "Copyright (c) 2016-2026 Brian Skinn and community contributors\n"
         "License: The MIT License\n\n"
         "Bug reports & feature requests:"
         " https://github.com/bskinn/sphobjinv\n"
