3c40df2aad401fab7d9ad2d6ef5114beba9f1f78
Brian Skinn <brian.skinn@gmail.com>
2026-03-22 01:15:55 -0400

    Add some pragma: no cover
    Some for windows; some for paths that are only reached
    when tests fail.

--- tests/conftest.py
+++ tests/conftest.py
@@ -155,7 +155,7 @@ def scratch_path(tmp_path, res_path, misc_info, is_win, unix2dos):
     # With the conversion of resources/objects_attrs.txt to Unix EOLs in order to
     # provide for a Unix-testable sdist, on Windows systems this resource needs
     # to be converted to DOS EOLs for consistency.
-    if is_win:
+    if is_win:  # pragma: no cover
         win_path = tmp_path / f"{scr_base}{misc_info.Extensions.DEC.value}"
         win_path.write_bytes(unix2dos(win_path.read_bytes()))
 
@@ -217,7 +217,7 @@ def sphinx_load_test(sphinx_ifile_load):
         """Perform the 'live' inventory load test."""
         try:
             sphinx_ifile_load(path)
-        except Exception as e:  # noqa: PIE786
+        except Exception as e:  # noqa: PIE786  pragma: no cover
             # An exception here is a failing test, not a test error.
             pytest.fail(e)
 
@@ -262,7 +262,7 @@ def run_cmdline_test(monkeypatch):
             except SystemExit as e:
                 retcode = e.args[0]
                 ok = True
-            else:
+            else:  # pragma: no cover
                 ok = False
 
         # Do all pytesty stuff outside monkeypatch context
@@ -284,7 +284,7 @@ def decomp_cmp_test(misc_info, is_win, unix2dos):
         res_bytes = Path(misc_info.res_decomp_path).read_bytes()
         tgt_bytes = Path(path).read_bytes()  # .replace(b"\r\n", b"\n")
 
-        if is_win:
+        if is_win:  # pragma: no cover
             # Have to explicitly convert these newlines, now that the
             # tests/resource/objects_attrs.txt file is marked 'binary' in
             # .gitattributes
